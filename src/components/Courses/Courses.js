29f571d46bec409c739e3cec5804848fbdd11e4a
Josh Hobson <joshuahobson1995@gmail.com>
2024-04-16 12:15:15 -0600

    courses

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './Courses.css'
+
+
+function Courses() {
+  return (
+<div class="courses_outer_shell">
+<div class="courses_top_background">
+<div class="courses_top">
+<h2>Relational Courses</h2>
+<div class="courses_top_inner">
+<p>Discover the key to meaningful connections and personal growth with our Relational Literacy Online Academy.</p>
+</div>
+</div>
+</div>
+<div class="courses_mid_page">
+<div class="teaching_with_relational_mind">
+<div class="teaching_with_relational_mind_top">
+<h3>Teaching with the Relational Mind</h3>
+<p>
+Relational work is very personal, and every individual will work relationally in a different and subjective way. This course will help you bring your authentic self to your teaching. Discovering who you are relationally can help you then foster that discovery in others. Participants will engage in an in-depth analysis of their personal relational mindset, as they gain knowledge on how to practice and maintain this. A relational mindset will help any educator increase the engagement of learners, families, and everyone within a school’s orbit.
+</p></div>
+<div class="teaching_relational_mind_iframe_container"><iframe title="Teaching with the Relational Mind" src="https://www.learndesk.us/class/4592070314426368/embed"></iframe>
+<div class="teaching_relational_mind_text">
+<h5>Teaching with the Relational Mind</h5>
+
+<span>by Dr. George Otero </span>
+ <p>Duration: 7hrs </p>
+
+<p>Included: 1 video, 9 documents, 1 quiz, 9 lessons and 8 assignments </p>
+
+<p> Level: Beginner Pricing: $99.99 (90 day access) </p>
+<a href="https://www.learndesk.us/class/4592070314426368/relational-teaching?ref=embed"> <button class="teaching_relational_mind_button">Buy Now</button> </a></div>
+</div>
+</div>
+<div class="leading_with_relational_mind">
+<div class="leading_with_relational_mind_top">
+<h3>Leading with the Relational Mind</h3>
+
+Participants in this course will learn about the values of fostering healthy relationships, utilizing our unique discovery process that builds positive relationships between families, schools, and communities. New strategies for when and how to foster those healthy leader-learner relationships will be shared and resources for leaders to use in the future will be provided.</div>
+<div class="leading_relational_mind_iframe_container"><iframe title="Leading with the Relational Mind" src="https://www.learndesk.us/class/5982348278497280/embed"></iframe>
+<div class="leading_relational_mind_text">
+<h5>Leading with the Relational Mind</h5>
+
+<span>by Dr. George Otero</span>
+<p> Duration: 7hrs </p>
+ <p>Included: 16 documents, 8 lessons and 14 assignments</p>
+ <p>Level: Beginner Pricing: $99.99 (90 day access) </p>
+<a href="https://www.learndesk.us/class/5982348278497280/relationalmind?ref=embed"> <button class="leading_relational_mind_button">Buy Now</button> </a></div>
+</div>
+</div>
+<div class="living_with_relational_mind">
+<div class="living_with_relational_mind_top">
+<h3>Living with the Relational Mind</h3>
+
+Participants in this course will immerse themselves in readings, discussions, as well as hands-on assignments that will start them down a new pathway. Utilizing relational strategies in everyday life is sure to produce positive change in the lives of anyone who gives it an honest try!</div>
+<div class="living_relational_mind_iframe_container"><iframe title="Living With the Relational Mind" src="https://www.learndesk.us/class/5555669782495232/embed"></iframe>
+<div class="living_relational_mind_text">
+<h5>Living with the Relational Mind</h5>
+
+<span>by Dr. George Otero </span> 
+<p>Duration: 7hrs </p>
+<p> Included: 11 documents, 1 quiz, 12 lessons and 11 assignments </p>
+<p> Level: Beginner Pricing: $99.99 (90 day access) </p>
+<a href="https://www.learndesk.us/class/5555669782495232/relationallife?ref=embed"> <button class="living_relational_mind_button">Buy Now</button> </a></div>
+</div>
+</div>
+</div>
+</div>
+  )
+}
+
+export default Courses
