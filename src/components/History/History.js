323e66b5b37a157ec94b1f1f2b6ce1db0a595c5f
Josh Hobson <joshuahobson1995@gmail.com>
2024-04-16 12:46:25 -0600

    update history

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -2,91 +2,88 @@ import React from 'react';
 
 const History = () => {
   return (
-    <div>
-      <div className="history2_outer_shell">
-        <div className="history_top_background">
-          <div className="history_top">
-            <h2>CRL History</h2>
-            <div className="history_top_inner">
-              {/* Accompany us on the journey through our past, and learn about who we are and where we come from. */}
+    <>
+      <div className="history">
+        <div className="history2_outer_shell">
+          <div className="history_top_background">
+            <div className="history_top">
+              <h2>CRL History</h2>
+              <div className="history_top_inner">
+                <p>Accompany us on the journey through our past, and learn about who we are and where we come from.</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="our_story">
-          <h2>Our Story</h2>
-        </div>
-        <div className="overlay_history">
-          <div className="timeline_image_container"><img className="timeline_image" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/HistoryBig.png" alt="" /></div>
-          <div className="our_history_div">
-            <h3>Our History</h3>
-            <p>
-              {/* Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. */}
-            </p>
+          <div className="our_story">
+            <h2>Our Story</h2>
           </div>
-          <div className="southwest_to_outback_div">
-            <h3>Southwest to Outback</h3>
-            <p>
-              {/* Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. */}
-            </p>
-          </div>
-          <div className="new_mexico_div">
-            <h3>New Mexico</h3>
-            <p>
-              {/* Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. */}
-            </p>
-          </div>
-        </div>
-
-        <div className='more_history'>
-          <h2>Space for more about George and the History of the Center</h2>
-          <img className="more_history_photo" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg" alt="" />
-          <p>
-            {/* Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. */}
-          </p>
-        </div>
-      </div>
-
-      <div className="mobile_history_outer_container">
-        <div className="mobile_history_top_background">
-          <div className="mobile_history_top">
-            <h2>CRL History</h2>
-            <div className="mobile_history_top_inner">
-              {/* Accompany us on the journey through our past, and learn about who we are and where we come from. */}
+          <div className="overlay_history">
+            <div className="timeline_image_container">
+              <img className="timeline_image" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/HistoryBig.png" alt="" />
             </div>
-          </div>
-        </div>
-        <div className="mobile_vertical_line"></div>
-        <div className="mobile_history_mid_page">
-          <div className="mobile_history_our_history">
-            <div className="mobile_history_our_history_inner">
+            <div className="our_history_div">
               <h3>Our History</h3>
-              <img className="mobile_history_yellow_paint" src="https://dev.relationalearning.com/wp-content/uploads/2024/02/paintbrush2-e1709075164364.png" alt="" />
-              {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit... */}
+              <p>The Center’s history is embedded in the sacred landscape of Northern New Mexico.  For more than 20 years the Center formed its mission and practice while based at the Historic Mabel Dodge Luhan House in Taos, New Mexico.  As a multicultural and multi-age living and learning center, Dr. Otero and associates hosted and facilitated hundreds of workshops, retreats, and conferences on Global and Multicultural Education, New Mexico History and Culture. We at CRL have always been pushing against the standard paradigm of education. We have been reforming the culture of teaching, learning and leading since the 70’s beginning with global, multicultural, diversity and inclusion issues, emerging into a focus on relational learning and community leadership,and  parent and community engagement across England, Australia and New Mexico. Most recently the focus since 2015 has been relational leadership development.</p>
             </div>
-            <img className="mobile_our_history_image" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/ourhistory.png" alt="" />
+            <div className="southwest_to_outback_div">
+              <h3>Southwest to Outback</h3>
+              <p>  Our pioneering work reflects the changing global context. First, we focused on building  American’s awareness of the global interdependence in which we all live. Learning and accepting our connectedness in the face of difference, conflicts and challenges has led us to assist diverse school communities across the US, Australia and England to develop a relational mindset and the corresponding skills and strategies that promote equity, tolerance, good will and interconnection.   As a result of working in so many different communities  we have developed new ways for people to connect and relate to each other.   A powerful expression of our unique approach has been the 20 years of learning journeys between the people of Australia and New Mexico. </p>
+            </div>
+            <div className="new_mexico_div">
+              <h3>Onward</h3>
+              <p>As we move through the impacts of isolation and disconnection created by the global pandemic and the emerging challenges of AI and social media, many now realize we are not meant to live disconnected, either from ourselves and each other. More people than ever are wanting a relational approach to life to counter the acceleration of anxiety, alienation, loneliness and apathy.  As we move forward we have adapted, refined and reformed our approach to best suit the newly emerging ways we live and work together.</p>
+            </div>
           </div>
-          <div className="mobile_southwest_to_outback">
-            <h3>Southwest to Outback</h3>
-            <img className="mobile_history_teal_paint" src="https://dev.relationalearning.com/wp-content/uploads/2024/02/paintbrush2-51.png" alt="" />
-            {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit... */}
-            <img className="mobile_our_history_image2" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/swtooutback.png" alt="" />
-          </div>
-          <div className="mobile_new_mexico_end">
-            <h3>New Mexico Based</h3>
-            <img className="mobile_history_nm_end" src="https://dev.relationalearning.com/wp-content/uploads/2024/02/paintstroke-5.png" alt="" />
-            {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit... */}
-            <img className="mobile_our_history_image3" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/newmexicotrue.png" alt="" />
-          </div>
-          <div className='m-more_history'>
+          <div className="more_history">
             <h2>Space for more about George and the History of the Center</h2>
-            <img className="m-more_history_photo" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg" alt="" />
-            <p>
-              {/* Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. */}
-            </p>
+            <div className='more_history_photo_container'>
+              <img className="more_history_photo" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg" alt="" />
+            </div>
+            <p>Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed.</p>
           </div>
         </div>
       </div>
-    </div>
+      <div className="mobile_history">
+        <div className="mobile_history_outer_container">
+          <div className="mobile_history_top_background">
+            <div className="mobile_history_top">
+              <h2>CRL History</h2>
+              <div className="mobile_history_top_inner">Accompany us on the journey through our past, and learn about who we are and where we come from.</div>
+            </div>
+          </div>
+          <div className="mobile_vertical_line"> </div>
+          <div className="mobile_history_mid_page">
+            <div className="mobile_history_our_history">
+              <div className="mobile_history_our_history_inner">
+                <h3>Our History</h3>
+                <p>The Center’s history is embedded in the sacred landscape of Northern New Mexico.  For more than 20 years the Center formed its mission and practice while based at the Historic Mabel Dodge Luhan House in Taos, New Mexico.  As a multicultural and multi-age living and learning center, Dr. Otero and associates hosted and facilitated hundreds of workshops, retreats, and conferences on Global and Multicultural Education, New Mexico History and Culture. We at CRL have always been pushing against the standard paradigm of education. We have been reforming the culture of teaching, learning and leading since the 70’s beginning with global, multicultural, diversity and inclusion issues, emerging into a focus on relational learning and community leadership,and  parent and community engagement across England, Australia and New Mexico. Most recently the focus since 2015 has been relational leadership development.</p>
+              </div>
+              <div className="p-u-container">
+                <img className="mobile_our_history_image" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/ourhistory.png" alt="" />
+              </div>
+            </div>
+            <div className="mobile_southwest_to_outback">
+              <h3>Southwest to Outback</h3>
+              <p> Our pioneering work reflects the changing global context. First, we focused on building  American’s awareness of the global interdependence in which we all live. Learning and accepting our connectedness in the face of difference, conflicts and challenges has led us to assist diverse school communities across the US, Australia and England to develop a relational mindset and the corresponding skills and strategies that promote equity, tolerance, good will and interconnection.   As a result of working in so many different communities  we have developed new ways for people to connect and relate to each other.   A powerful expression of our unique approach has been the 20 years of learning journeys between the people of Australia and New Mexico. </p>
+              <div className="p-u-container">
+                <img className="mobile_our_history_image2" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/swtooutback.png" alt="" />
+              </div>
+            </div>
+            <div className="mobile_new_mexico_end">
+              <h3>New Mexico Based</h3>
+              <p> As we move through the impacts of isolation and disconnection created by the global pandemic and the emerging challenges of AI and social media, many now realize we are not meant to live disconnected, either from ourselves and each other. More people than ever are wanting a relational approach to life to counter the acceleration of anxiety, alienation, loneliness and apathy.  As we move forward we have adapted, refined and reformed our approach to best suit the newly emerging ways we live and work together. </p>
+              <div className="p-u-container">
+                <img className="mobile_our_history_image3" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/newmexicotrue.png" alt="" />
+              </div>
+            </div>
+            <div className="m-more_history">
+              <h2>Space for more about George and the History of the Center</h2>
+              <img className="m-more_history_photo" src="https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg" alt="" />
+              <p>Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed. Fusce id ipsum et massa rutrum mattis quis non ligula. Maecenas vitae gravida massa. Nulla nisi dolor, imperdiet a enim non, vehicula varius enim. Ut ut nulla lobortis, luctus augue id, tincidunt ligula. Praesent tempor diam velit, in pellentesque dui tristique sed.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
